3cbf8de145ce9a09217a1caac06f1fb6e0acc5f8
Hampus Zätterberg <hampus.zatterberg@mobenga.com>
2016-01-27 20:13:34 +0100

    Initial Commit

---
+++ Samverkan112Tests/Samverkan112Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Samverkan112Tests.swift
+//  Samverkan112Tests
+//
+//  Created by Hampus Zätterberg on 2016-01-27.
+//  Copyright © 2016 hampus. All rights reserved.
+//
+
+import XCTest
+@testable import Samverkan112
+
+class Samverkan112Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
